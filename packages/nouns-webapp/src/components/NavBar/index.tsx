862533dc2620e5bd9cf928543de5ce22e53d8d1b
defnullx <defnullx@outlook.com>
2022-08-18 17:00:30 -0300

    Fixing warnings

--- packages/nouns-webapp/src/components/NavBar/index.tsx
+++ packages/nouns-webapp/src/components/NavBar/index.tsx
@@ -5,8 +5,8 @@ import { useEtherBalance } from '@usedapp/core';
 import { useHistory } from 'react-router';
 import { Link } from 'react-router-dom';
 import { Nav, Navbar, Container } from 'react-bootstrap';
-import testnetNoun from '../../assets/testnet-noun.png';
-import config, { CHAIN_ID } from '../../config';
+
+
 import { utils } from 'ethers';
 import { buildEtherscanHoldingsLink } from '../../utils/etherscan';
 import { ExternalURL, externalURL } from '../../utils/externalURL';
