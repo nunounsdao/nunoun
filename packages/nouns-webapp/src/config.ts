c7b03cba07cd9e9aa3f8314d1c3f77950035553a
defnullx <defnullx@outlook.com>
2022-08-21 21:27:41 -0300

    mainnet subgraph

--- packages/nouns-webapp/src/config.ts
+++ packages/nouns-webapp/src/config.ts
@@ -65,7 +65,7 @@ const app: Record<SupportedChains, AppConfig> = {
   [ChainId.Mainnet]: {
     jsonRpcUri: createNetworkHttpUrl('mainnet'),
     wsRpcUri: createNetworkWsUrl('mainnet'),
-    subgraphApiUri: 'https://api.thegraph.com/subgraphs/name/nunounsdao/nunouns-subgraph-rinkeby',
+    subgraphApiUri: 'https://api.thegraph.com/subgraphs/name/nunounsdao/nunouns-subgraph-mainnet',
     enableHistory: process.env.REACT_APP_ENABLE_HISTORY === 'true',
   },
   [ChainId.Hardhat]: {
